793cabbb9476e79d68ccf7a1f1c8c54ad501066d
Graham Pugh <5802725+grahampugh@users.noreply.github.com>
2026-02-24 08:29:28 +0000

    bump to 41.1

--- erase-install.sh
+++ erase-install.sh
@@ -39,7 +39,7 @@ script_name="erase-install"
 pkg_label="com.github.grahampugh.erase-install"
 
 # Version of this script
-version="41.0"
+version="41.1"
 
 # Directory in which to place the macOS installer. Overridden with --path
 installer_directory="/Applications"
